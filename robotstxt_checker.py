1ebe464ed91daa82d4fc4c1499bccb87f335aab4
Chris Mastris <cmastris@hotmail.com>
2020-11-14 12:33:13 +0000

    Update and add TODOs throughout file

--- robotstxt_checker.py
+++ robotstxt_checker.py
@@ -1,27 +1,27 @@
-""" Download and report changes in multiple website robots.txt files.
-
-TODO: Complete core functionality.
-TODO: Develop email alerts.
-TODO: Provide more details about changes using difflib.
-TODO: Allow for multiple emails per website.
-TODO: Add noindex checks for priority pages.
-TODO: (Maybe) Use a database to manage monitored sites (creation, deletion, email changes).
-"""
-
-# TODO: Import Beautiful Soup for scraping
+""" Download and report changes in multiple website robots.txt files."""
+# TODO: Complete core functionality
+# TODO: Write tests and fix bugs
+# TODO: Provide more details about changes using difflib
+# TODO: Allow for multiple emails per website
+# TODO: Add noindex checks for priority pages
+# TODO: Consider using a DB to manage monitored sites (creation, deletion, email/name changes)
 
 
 # List of tuples containing each monitored website name, URL, and owner email
-# TODO: use CSV file instead
-monitored_sites = [("https://www.example.com/", "Test Site", "test@hotmail.com"),
+# TODO: Use CSV file instead and init in ManageWebsiteChecks
+monitored_sites = [
+    ("https://www.example.com/", "Test Site", "test@hotmail.com"),
                    ]
 
 # File location of master log which details check and change history
+# TODO: Add master log location, create file if it doesn't exist (check using os.path)
 master_log = None
 
 
 class ManageWebsiteChecks:
     """Manages all monitored website checks."""
+    # TODO: complete class and method documentation
+
     def __init__(self, sites):
         """Initialise RobotsCheck instances and store them in a list."""
         self.sites = sites
@@ -30,7 +30,6 @@ class ManageWebsiteChecks:
         """Iterate over all RobotsCheck instances to run change checks and reports."""
         for site_check in self.sites:
             url, name, email = site_check
-            # TODO: record date/time and pass to report
             check = RobotsCheck(url)
             check.run_check()
             print("Robots.txt checked: " + check.url)
@@ -45,7 +44,7 @@ class ManageWebsiteChecks:
                 report = NoChangeReport(check, name, email)
 
             report.create_reports()
-            print("Reports complete: " + report.url)
+            print("Report complete: " + report.url)
 
         print("All checks and reports complete.")
 
@@ -58,17 +57,17 @@ class RobotsCheck:
     any differences between the records. The results (change, no change, first run, err)
     are assigned as attributes and the instance is returned.
 
-    TODO: complete documentation
     Attributes:
         url (str): the absolute URL of the website homepage, with a trailing slash.
         first_run (): if this is the first check of the website's file.
     """
+    # TODO: complete class and method documentation
 
     def __init__(self, url):
         self.url = url
         self.err_message = None
         self.file_change = False
-        # Try to assign existing files to self
+        # TODO: Try to assign existing files to self
         # Otherwise, if they don't exist, create/assign files and set first_run = True
         self.first_run = False
         self.log_file = "test log"
@@ -82,7 +81,7 @@ class RobotsCheck:
         print("Method run_check called.")
         try:
             extraction = self.download_robotstxt()
-        except:  # Add specific errors and messages
+        except:  # TODO: Add specific errors and messages
             self.err_message = "something"
             return self
 
@@ -116,13 +115,20 @@ class RobotsCheck:
 class WebsiteReport:
     """Reports the robots.txt check result for a single website.
 
-    TODO: complete documentation
-    TODO: Create subclasses for no change, change, first run, and error
     Attributes:
         name (str): the website's name identifier.
         url (str): the absolute URL of the website homepage, with a trailing slash.
         email (str): the email address of the owner, who will receive alerts.
     """
+    # TODO: complete class and method documentation
+    # TODO: Create subclasses for no change, change, first run, and error
+    # TODO: Move file_change, first_run, and err_message attributes to subclasses
+    # TODO: Add method to get date/time
+    # TODO: Add methods to print results to the console
+    # TODO: Add methods to update the master log
+    # TODO: Add methods to update the website log
+    # TODO: Add methods to send an email report
+
     def __init__(self, website, name, email):
         self.url = website.url
         self.file_change = website.file_change
@@ -140,15 +146,15 @@ class WebsiteReport:
 
 
 class NoChangeReport(WebsiteReport):
-    # TODO: Complete docs and class
+    # TODO: Complete documentation and add methods
     def __init__(self, website, name, email):
-        WebsiteReport.__init__(self, website, name, email)
+        WebsiteReport.__init__(self, website, name, email)  # TODO: Change to use super
 
 
 class ChangeReport(WebsiteReport):
-    # TODO: Complete docs and class
+    # TODO: Complete documentation and add methods
     def __init__(self, website, name, email):
-        WebsiteReport.__init__(self, website, name, email)
+        WebsiteReport.__init__(self, website, name, email)  # TODO: Change to use super
         self.old_content = website.old_content
         self.new_content = website.new_content
 
