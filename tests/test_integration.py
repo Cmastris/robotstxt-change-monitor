66327ff82a6c784af0a3f22bac97568964dd95fc
Chris Mastris <cmastris@hotmail.com>
2026-06-14 15:27:57 +0100

    Add test: end-to-end for first site check

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import shutil
 
 
 def get_previous_minute_timestamp():
@@ -37,6 +39,63 @@ def test_app_runs_without_fatal_error(monkeypatch):
     # and that the main log summary is present
     assert (timestamp_now in main_log_summary) or (timestamp_prev_minute in main_log_summary)
     assert "Checks and reports complete" in main_log_summary
+
+
+def test_first_run(monkeypatch):
+    """Tests behaviour/output if the first robots.txt check."""
+    # Use modified testing variables in config.py
+    monkeypatch.setenv("ROBOTS_MONITOR_ENV", "test")
+    
+    # Import after monkeypatch
+    from app.main import main
+    from app.config import MAIN_LOG, PATH
+    from app.logs import get_timestamp
+
+    # Check a single site
+    site_dir = PATH + "data/www.bbc.co.uk"
+    def mock_sites_from_file(file):
+        return [['https://www.bbc.co.uk/', 'The BBC', ''],]
+
+    monkeypatch.setattr("app.main.sites_from_file", mock_sites_from_file)
+
+    # Delete the existing site directory to simulate a new site
+    if os.path.isdir(site_dir):
+        shutil.rmtree(site_dir)
+
+    main()
+
+    # Log timestamps should contain the current minute or the minute before
+    timestamp_now = get_timestamp()
+    timestamp_prev_minute = get_previous_minute_timestamp()
+
+    # Retrieve log summary lines
+    with open(site_dir + "/log.txt", 'r') as f:
+        site_log_summary = f.readlines()[-1]
+
+    with open(MAIN_LOG, 'r') as f:
+        main_log_summary = f.readlines()[-4]
+
+    # Check that the most recent log summaries are for the recent run
+    assert (timestamp_now in site_log_summary) or (timestamp_prev_minute in site_log_summary)
+    assert (timestamp_now in main_log_summary) or (timestamp_prev_minute in main_log_summary)
+
+    # Check that the log summaries are accurate
+    expected_site_log_summary = "First run: https://www.bbc.co.uk/. " \
+                                "First successful check of robots.txt file."
+    assert expected_site_log_summary in site_log_summary
+    
+    expected_main_log_summary = "No change: 0. Change: 0. First run: 1. Error: 0."
+    assert expected_main_log_summary in main_log_summary
+
+    # Check that snapshot files were created
+    assert "Snapshot.txt" in os.listdir(site_dir + "/snapshots")[0]
+    sorted_program_files = sorted(os.listdir(site_dir + "/program_files"))
+    assert sorted_program_files == ["new_file.txt", "old_file.txt"]
+
+    with open(site_dir + "/program_files/new_file.txt", 'r') as f:
+        new_file_text = f.read()
+
+    assert len(new_file_text) > 0
 
 
 def test_no_change(monkeypatch):
